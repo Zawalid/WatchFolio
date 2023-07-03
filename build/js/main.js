0576c9b744526afae51491738198e9e97c8e150e
Walid Zakan <walid.zakan@gmail.com>
2023-07-03 02:03:15 +0100

    New tool coming soon

---
+++ build/js/main.js
@@ -0,0 +1,99 @@
+"use strict";
+
+//! Search functionality
+const container = document.getElementById("search_results");
+const searchInput = document.getElementById("search_input");
+const searchBtn = document.getElementById("search_button");
+
+const displayResults = async (query) => {
+  // Fetch the data
+  const res = await fetch(` https://api.tvmaze.com/search/shows?q=${query}`);
+  //   Convert the response to json
+  const data = await res.json();
+  console.log(data);
+  // If there are no results
+  const noResults = `
+  <div class="flex flex-col items-center justify-center">
+  <img src="./imgs/no result search icon.png" alt="" class="h-64 w-64" />
+  <h2 class="mb-3 text-xl font-bold text-thirdAccent">No Result Found</h2>
+  <h3 class="font-semibold text-textColor2 text-center">
+    We couldn't find any show matching your search
+  </h3>
+</div>
+  `;
+  if (data.length === 0) {
+    container.innerHTML = noResults;
+    return;
+  }
+  // Clear the container
+  container.innerHTML = "";
+  // Loop through the data array and create the each show element
+  for (const obj of data) {
+    // Get the show object from the data array
+    const { show } = obj;
+    // Create the elements
+    const image = document.createElement("img");
+    const div = document.createElement("div");
+    const a = document.createElement("a");
+    // Create the info div
+    const info = `
+<div
+        class="blur-[60px] top-0 absolute -z-10 flex h-full w-full flex-col gap-3 bg-dark p-3 transition-all duration-[1s] group-hover:z-10 group-hover:bg-dark group-hover:bg-opacity-50 group-hover:blur-0"
+      >
+        <div
+          class="w-fit rounded-lg bg-thirdAccent px-3 text-sm font-semibold text-textColor"
+        >
+          ${show.genres[0] || "No genre"}
+        </div>
+        <div
+          class="flex w-fit items-center gap-1 rounded-lg bg-secondaryAccent px-3 text-sm font-semibold text-textColor"
+        >
+          ${
+            show.rating.average || "Not rated"
+          } <i class="fa-solid fa-star text-primaryAccent"></i>
+        </div>
+        <h3 class="mt-auto font-logo text-lg font-bold text-textColor">
+          ${show.name}
+        </h3>
+      </div>
+`;
+    // Set the attributes
+    image.src = show.image?.medium || "./imgs/placeholder.png";
+    image.alt = show.name;
+    image.className =
+      "w-full h-full object-cover rounded-md shadow-shadow1 mb-2";
+    div.className = "w-[210px] h-[270px] peer group relative overflow-hidden";
+    a.href = `#${show.id}`;
+    // Append the elements to the div and anchor
+    div.append(image);
+    div.insertAdjacentHTML("beforeend", info);
+    a.append(div);
+    // Append the anchor to the container
+    container.append(a);
+  }
+};
+//* Search function
+const search = (e) => {
+  // If the user presses enter or clicks the search button
+  if (e.key === "Enter" || e.type === "click") {
+    container.classList.add("grid");
+    container.classList.remove("hidden");
+    // Scroll to the container
+    container.scrollIntoView({ behavior: "smooth" });
+    // Display the data
+    displayResults(searchInput.value);
+  }
+};
+//* If the search input is empty
+const nothingToSearch = () => {
+  if (searchInput.value === "") {
+    container.classList.add("hidden");
+    container.classList.remove("grid");
+    container.innerHTML = "";
+  }
+};
+//* Event listeners
+[search, nothingToSearch].forEach((func) => {
+  searchInput.addEventListener("keyup", func);
+  searchBtn.addEventListener("click", func);
+});
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/**/*.{html,js}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "1.5rem",
+        screens: {
+          xs: "1000px",
+          lg: "1125px",
+          xl: "1280px",
+          "2xl": "1536px",
+        },
+      },
+      colors: {
+        primaryAccent: "#FFC107",
+        secondaryAccent: "#F44336",
+        thirdAccent: "#03A9F4",
+        textColor: "#fff",
+        textColor2: "#D4D4D4",
+        dark: "#130F40",
+      },
+      backgroundImage: {
+        background: "url('../imgs/bg.jpg')",
+        gradient: "linear-gradient(306deg, #000 0%, #0B1531 100%)",
+      },
+      fontFamily: {
+        logo: ["Bona Nova", "serif"],
+      },
+      boxShadow: {
+        shadow1: "-7px 11px 20px #000000a1",
+      },
+    },
+  },
+  plugins: [],
+};
