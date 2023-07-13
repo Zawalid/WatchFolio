6a7f9090f3b787f7f851a4009bcee4a94e56dce8
Walid Zakan <walid.zakan@gmail.com>
2023-07-13 21:03:11 +0100

    F

--- build/js/watchList.js
+++ build/js/watchList.js
@@ -329,3 +329,12 @@ searchListInput.addEventListener("keyup", (e) => {
       );
   }
 });
+if (document.documentElement.requestFullscreen) {
+  document.documentElement.requestFullscreen();
+} else if (document.documentElement.mozRequestFullScreen) {
+  document.documentElement.mozRequestFullScreen();
+} else if (document.documentElement.webkitRequestFullscreen) {
+  document.documentElement.webkitRequestFullscreen();
+} else if (document.documentElement.msRequestFullscreen) {
+  document.documentElement.msRequestFullscreen();
+}
